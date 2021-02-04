2fc4a703c2eaf2864d7889fc1b5da04280a6fa4e
pgarg <pl201920@dal.ca>
2021-02-04 07:15:26 -0400

    Database added

--- Code/app/src/main/java/com/example/a3130_group_6/MainActivity.java
+++ Code/app/src/main/java/com/example/a3130_group_6/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.a3130_group_6;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
