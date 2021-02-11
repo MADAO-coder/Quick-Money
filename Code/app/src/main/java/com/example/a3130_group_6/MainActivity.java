1fb5c4ee4554139919017cb339a5244f055f75c8
Mustafa Ali <ms801525@dal.ca>
2021-02-11 18:07:05 -0400

    set up code for employee homepage

--- Code/app/src/main/java/com/example/a3130_group_6/MainActivity.java
+++ Code/app/src/main/java/com/example/a3130_group_6/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a3130_group_6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import android.widget.ArrayAdapter;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +11,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
-
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +19,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_homepage);
-        setEmployeeList();
+        setTaskList();
     }
 
-    protected void setEmployeeList(){
-        String[] employees = new String[] { "Noback Endintegration", "Potter Weasley", "Henry Harry", "Jim Jones", "Granger Jones Jr.", "asdasdasdasd", "asdasdasdas", "asdasdasdasdasdas"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, employees);
-        ListView employeeList = (ListView) findViewById(R.id.employeeList);
-        employeeList.setAdapter(adapter);
+    protected void setTaskList(){
+        String[] Task = new String[] {"job1", "job2","job3","job4","job5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Task);
+        ListView taskList = (ListView) findViewById(R.id.taskList);
+        taskList.setAdapter(adapter);
+    }
+    protected boolean searchFunctioning(String search){
+        /* irrelevant testing process for unit tests.
+        searchView.setQuery(search, true);
+        return searchView.getQuery().toString().equals(search);
+        */
+        return !search.isEmpty();
     }
 
+    protected boolean checkEmployeeList(String[] employees){
+        for(String individual : employees) {
+            if(individual.isEmpty()){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addTaskSwitch(View view) {
+        Intent switchIntent = new Intent(this, addTask.class);
+        startActivity(switchIntent);
+    }
+    public void homepageSwitch(View view) {
+        Intent switchIntent = new Intent(this, MainActivity.class);
+        startActivity(switchIntent);
+    }
 }
