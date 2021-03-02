08f206ffb799e009ad5f58c2036fe32f3a1e893b
bryson <br441570@dal.ca>
2021-03-01 20:50:07 -0400

    :Variables must be tracked post login, so added routing. EditView for name label is null error.

--- Code/app/src/main/java/com/example/a3130_group_6/EmployerHomepage.java
+++ Code/app/src/main/java/com/example/a3130_group_6/EmployerHomepage.java
@@ -1,5 +1,6 @@
 package com.example.a3130_group_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuItemCompat;
 
@@ -12,9 +13,22 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
+import static com.example.a3130_group_6.loginPage.validEmployer;
+
 public class EmployerHomepage extends AppCompatActivity {
+
+    private List<Employee> employeeList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +38,35 @@ public class EmployerHomepage extends AppCompatActivity {
     }
 
     protected void setEmployeeList(){
-        String[] employees = new String[] { "Noback Endintegration", "Potter Weasley", "Henry Harry", "Jim Jones", "Granger Jones Jr.", "asdasdasdasd", "asdasdasdas", "asdasdasdasdasdas"};
+        DatabaseReference employeeRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://group-6-a830d-default-rtdb.firebaseio.com/Employee");
+        dbReadEmployer(employeeRef);
+        List<String> employees = new ArrayList<String>();
+        for (Employee employee : employeeList) {
+            employees.add(employee.getName() + "\t" + employee.getEmail() + "\t" + employee.getPassword() + "\t" + employee.getPhone() + "\t" + employee.getUserName());
+        }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, employees);
-        ListView employeeList = (ListView) findViewById(R.id.employeeList);
-        employeeList.setAdapter(adapter);
+        ListView employeeListView = (ListView) findViewById(R.id.employeeList);
+        employeeListView.setAdapter(adapter);
     }
+    public void dbReadEmployer(DatabaseReference db) {
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> employeeItr = dataSnapshot.getChildren().iterator();
+                //Read data from data base.
+                while (employeeItr.hasNext()) {
+                    Employee employee = employeeItr.next().getValue(Employee.class);
+                    employeeList.add(employee);
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });
+    }
     protected boolean searchFunctioning(String search){
         /* irrelevant testing process for unit tests.
         searchView.setQuery(search, true);
@@ -47,6 +84,11 @@ public class EmployerHomepage extends AppCompatActivity {
             }
         }
         return false;
+    }
+
+    public void profileSwitch(View view) {
+        Intent switchIntent = new Intent(this, EmployerProfile.class);
+        startActivity(switchIntent);
     }
 
     public void addTaskSwitch(View view) {
--- Code/app/src/main/java/com/example/a3130_group_6/EmployerProfile.java
+++ Code/app/src/main/java/com/example/a3130_group_6/EmployerProfile.java
@@ -2,6 +2,8 @@ package com.example.a3130_group_6;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,48 +14,54 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import static com.example.a3130_group_6.loginPage.validEmployer;
 
 public class EmployerProfile extends AppCompatActivity {
     DatabaseReference employerRef = null;
     private List<String> employer_userName_list = new ArrayList<>();//List to store password getting from db for Employee object
     private List<String> employer_password_list = new ArrayList<>();//List to store password getting from db for Employee object
     String biography;
+    EditText nameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        nameView = findViewById(R.id.editName);
         setContentView(R.layout.activity_employer_profile);
-    }
-
-    public void onCLick(View v){
         employerRef= FirebaseDatabase.getInstance().getReferenceFromUrl("https://group-6-a830d-default-rtdb.firebaseio.com/Employer");
-        dbReadEmployer(employerRef, biography);//Get data from database
+        dbReadEmployer(employerRef);//Get data from database
     }
 
     //code from loginPage
     //Read data from dataBase and make employers' userName and password into ArrayList.
-    public void dbReadEmployer(DatabaseReference db, String statusMessage){
+    public void dbReadEmployer(DatabaseReference db){
 
         db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Iterator<DataSnapshot> employerItr = dataSnapshot.getChildren().iterator();
                 //Read data from data base.
-
                 while (employerItr.hasNext()) {
                     Employer employer = employerItr.next().getValue(Employer.class);
-                    String employerUserName = employer.getUserName();
-                    String employerPassword = employer.getPassword();
-                    //if there is a tracked session, perhaps retrieve from there
-                    employer_userName_list.add(employerUserName);
-                    employer_password_list.add(employerPassword);
+                        //need to check against correct value to retrieve the correct location
+                        if (employer.getUserName().equals(validEmployer[0])){
+                            String username = employer.getUserName();
+                            String password = employer.getPassword();
+                            String phone = employer.getPassword();
+                            String email = employer.getPassword();
+                            String name = employer.getPassword();
+                            nameView.setText(name);
+                            String business = employer.getPassword();
+                            break;
+                    }
                 }
-                for(int i=0; i<employer_password_list.size(); i++){
-                    //need to check against correct value to retrieve the correct location
-                }
+
             }
 
             @Override
@@ -62,7 +70,6 @@ public class EmployerProfile extends AppCompatActivity {
 
             }
         });
-
 
     }
 }
