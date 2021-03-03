8fa1b6e1e1f974e7f8bb0355831a49888f8b564d
pgarg <pl201920@dal.ca>
2021-03-03 19:07:23 -0400

    UI Fixed - Pulkit

--- Code/app/src/main/java/com/example/a3130_group_6/EmployerHomepage.java
+++ Code/app/src/main/java/com/example/a3130_group_6/EmployerHomepage.java
@@ -36,7 +36,7 @@ public class EmployerHomepage extends AppCompatActivity {
         for(String individual : employees) {
             if(individual.isEmpty()){
                 return false;
-            }else{
+            } else {
                 return true;
             }
         }
