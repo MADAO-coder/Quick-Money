1bf5b7810efdfc0a8bbae8f0af370d82fcb2a074
bryson <br441570@dal.ca>
2021-03-08 15:32:30 -0400

    all listings will appear in a scrollable element - Bryson

--- Code/app/src/main/java/com/example/a3130_group_6/ListingHistory.java
+++ Code/app/src/main/java/com/example/a3130_group_6/ListingHistory.java
@@ -23,12 +23,15 @@ import static com.example.a3130_group_6.loginPage.validEmployer;
 
 public class ListingHistory extends AppCompatActivity {
     DatabaseReference employerRef = null;
+    FirebaseDatabase database;
     String fireRef;
     TextView NoListing = null;
-    List<Listing> listings;
+    List<Listing> listings = null;
     DataSnapshot employer;
     ListView listView=null;
     String[] listingsString;
+    DataSnapshot listingData;
+    Iterator<DataSnapshot> listingItr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +40,21 @@ public class ListingHistory extends AppCompatActivity {
         NoListing = findViewById(R.id.noListingMessage);
         listView = findViewById(R.id.listingHistoryView);
         listings = new ArrayList<>();
+        database = FirebaseDatabase.getInstance();
         fireRef =  "https://group-6-a830d-default-rtdb.firebaseio.com/Employer";
-        employerRef= FirebaseDatabase.getInstance().getReferenceFromUrl(fireRef);
+        employerRef= database.getReferenceFromUrl(fireRef);
         dbReadEmployer(employerRef);
-        retrieveListings(employerRef);
     }
     public void updateListing(){
-        if(listings!=null){
+        if(listings.size()>0){
             listingsString = new String[listings.size()];
             for(int i=0; i<listingsString.length; i++){
                 listingsString[i] = listings.get(i).getTaskTitle();
             }
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listingsString);
             listView.setAdapter(adapter);
+        }else{
+            NoListing.setVisibility(View.VISIBLE);
         }
     }
 
@@ -69,37 +74,14 @@ public class ListingHistory extends AppCompatActivity {
                         if(employer.getValue(Employer.class).getUserName().equals(validEmployer[0])) {
                             fireRef = fireRef + "/" + employer.getValue(Employer.class).getUserName() + "/Listing";
                             NoListing.setVisibility(View.INVISIBLE);
-                            employerRef= FirebaseDatabase.getInstance().getReferenceFromUrl(fireRef);
-                            break;
+                            employerRef= database.getReferenceFromUrl(fireRef);
+                            listingData = employer.child("Listing");
+                            listingItr = listingData.getChildren().iterator();
+                            while (listingItr.hasNext()) {
+                                listings.add(listingItr.next().getValue(Listing.class));
+                            }
                         }
-                    }else{
-                        //do something
-                        NoListing.setVisibility(View.VISIBLE);
                     }
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-
-            }
-        });
-    }
-    public void retrieveListings(DatabaseReference db){
-        // not iterating through the onDataChange or anything for some reason
-        db.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // below line retrieves all employer data
-                Iterator<DataSnapshot> dataItr = dataSnapshot.getChildren().iterator();
-                //Read listings, add to list
-                while (dataItr.hasNext()) {
-                    // need to get each listing, but their key value is a stupid unintelligible string
-                    listings.add(dataItr.next().child("Listing").child("-MUzCFONR80M1C9TRVxk").getValue(Listing.class));
-                    // shouldnt break, should properly retrieve each listing (based on some undefined criteria), instead of just one
-                    break;
                 }
                 updateListing();
             }
