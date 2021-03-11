526e9b07cdb9c9fbe387185d20d66394fe9847b7
bryson <br441570@dal.ca>
2021-03-11 10:50:25 -0400

    Started work by fixing employee homepage, all listings appear now - Bryson

--- Code/app/src/main/java/com/example/a3130_group_6/EmployeeHomepage.java
+++ Code/app/src/main/java/com/example/a3130_group_6/EmployeeHomepage.java
@@ -1,6 +1,8 @@
 package com.example.a3130_group_6;
 
 import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,22 +12,70 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.*;
 
+import static com.example.a3130_group_6.loginPage.validEmployer;
+
 public class EmployeeHomepage extends AppCompatActivity {
+    DatabaseReference employerRef;
+    DataSnapshot listingData;
+    Iterator<DataSnapshot> listingItr;
+    ArrayList<Listing> listings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_homepage);
-        setEmployeeList();
+        employerRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://group-6-a830d-default-rtdb.firebaseio.com/Employer");
+        listings = new ArrayList<>();
+        dbReadEmployees(employerRef, listings);
+
+    }
+    protected void setEmployerList(){
+        String[] listingsString = new String[listings.size()];
+        for(int i=0; i<listingsString.length; i++){
+            listingsString[i] = listings.get(i).getTaskTitle() + "\tStatus:" + listings.get(i).getStatus();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listingsString);
+        ListView taskList = (ListView) findViewById(R.id.TaskList);
+        taskList.setAdapter(adapter);
     }
 
-    protected void setEmployeeList(){
-        String[] employees = new String[] { "Noback Endintegration", "Potter Weasley", "Henry Harry", "Jim Jones", "Granger Jones Jr.", "asdasdasdasd", "asdasdasdas", "asdasdasdasdasdas"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, employees);
-        ListView employeeList = (ListView) findViewById(R.id.TaskList);
-        employeeList.setAdapter(adapter);
+    // need to set a listing
+    public void dbReadEmployees(DatabaseReference db, ArrayList<Listing> listings){
+        final DataSnapshot[] employer = new DataSnapshot[1];
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> employerItr = dataSnapshot.getChildren().iterator();
+                //Read data from data base.
+                while (employerItr.hasNext()) {
+                    employer[0] = employerItr.next();
+                    if(employer[0].hasChild("Listing")){
+                        // if employer location has Listings, add to the list
+                        listingData = employer[0].child("Listing");
+                        listingItr = listingData.getChildren().iterator();
+                        while (listingItr.hasNext()) {
+                            listings.add(listingItr.next().getValue(Listing.class));
+                        }
+                    }
+                }
+                setEmployerList();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });
+
     }
     public void homepageSwitch(View view) {
         Intent switchIntent = new Intent(this, EmployeeHomepage.class);
