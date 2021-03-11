395cd6621d7e5444a9fcabdfcaf6c4248cb997f8
bryson <br441570@dal.ca>
2021-03-11 15:43:24 -0400

    clicking a listing reroutes user intent to ListingApplicants page (this will be routed under the edit/listing details page Ty is creating) - Bryson

---
+++ Code/app/src/main/java/com/example/a3130_group_6/ListingApplicants.java
@@ -0,0 +1,14 @@
+package com.example.a3130_group_6;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListingApplicants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listing_applicants);
+    }
+}
