40f4384febf4dea00cf7b711a00c1451be7b2082
pgarg <pl201920@dal.ca>
2021-04-02 19:54:31 -0300

    Activity for showing employers that accepted the employee created + Espresso Tests for the page passed - Pulkit, Han

---
+++ Code/app/src/main/java/com/CSCI/a3130_group_6/EmployeePackage/EmployeeAcceptedListings.java
@@ -0,0 +1,16 @@
+package com.CSCI.a3130_group_6.EmployeePackage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.CSCI.a3130_group_6.R;
+
+public class EmployeeAcceptedListings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_accepted_listings);
+    }
+}
